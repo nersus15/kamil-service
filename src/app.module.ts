444b76e6ded4da5c34ab8a1471753efe26a1d886
fathurrahman <fathur.ashter15@gmail.com>
2019-11-28 00:26:26 +0800

    Add auth diretory with module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { KaderModule } from './kader/kader.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [KaderModule],
+  imports: [KaderModule, AuthModule],
 })
 export class AppModule { }
