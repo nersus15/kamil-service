f8c3becef6dafbb8af641836e898c512943b035f
fathurrahman <fathur.ashter15@gmail.com>
2019-11-28 01:22:29 +0800

    changed few things in the kader.module.ts and auth.module.ts file

--- src/kader/kader.module.ts
+++ src/kader/kader.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { KaderController } from './kader.controller';
 import { KaderService } from './kader.service';
-import { GoogleStrategy } from 'src/auth/google.strategy';
 
 @Module({
   controllers: [KaderController],
-  providers: [KaderService, GoogleStrategy]
+  providers: [KaderService]
 })
 export class KaderModule { }
