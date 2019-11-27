f1f8fee5933d411fe886115955872ea0850b09e9
fathurrahman <fathur.ashter15@gmail.com>
2019-11-28 00:40:35 +0800

    Import the GoogleStrategy as a provider in the AuthModule

--- src/kader/kader.module.ts
+++ src/kader/kader.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KaderController } from './kader.controller';
 import { KaderService } from './kader.service';
+import { GoogleStrategy } from 'src/auth/google.strategy';
 
 @Module({
   controllers: [KaderController],
-  providers: [KaderService]
+  providers: [KaderService, GoogleStrategy]
 })
-export class KaderModule {}
+export class KaderModule { }
