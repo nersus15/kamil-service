c407a5411de3511ff8d45557f689cc874d47e347
fathurrahman <fathur.ashter15@gmail.com>
2019-12-03 10:59:39 +0800

    change response for controller /auth/google/callback

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,15 +14,26 @@ export class AuthController {
     googleLoginCallback(@Req() req, @Res() res) {
         // handles the Google OAuth2 callback
         const jwt: string = req.user.jwt;
-        if (jwt)
-            res.redirect('http://localhost:4200/login/succes/' + jwt);
-        else
-            res.redirect('http://localhost:4200/login/failure');
+        let response;
+        if (jwt) {
+            response = {
+                message: "Login Success",
+                token: jwt
+            };
+        }
+
+        else {
+            response = {
+                message: "Login Failed",
+                token: null
+            };
+        }
+        res.send(response);
+
     }
     @Get('protected')
     @UseGuards(AuthGuard('jwt'))
-    protectedResource()
-    {
+    protectedResource() {
         return 'JWT is working!';
     }
 }
